424a083b8879b85735acb04efa3bf0989c2f152f
andrii00 <kachmarik00@gmail.com>
2021-07-13 16:38:26 +0300

    lesson 2

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,23 +1,32 @@
 import './App.css';
 import {useEffect, useState} from "react";
-import {getUsers,getUser} from "../sevisec/Api";
+import {getUsers, getUser} from "../sevisec/Api";
 import Users from "../users/Users";
 
 export function App() {
 
+    let [user, setUser] = useState([])
+
     let appFn = (id) => {
-        getUser(id).then(value => console.log(value.data))
+        {
+            getUser(id).then(value => setUser((value.data)))
+        }
     }
 
     let [users, setUsers] = useState([]);
 
     useEffect(() => {
         getUsers().then(value => setUsers(value.data))
-    },[]);
+    }, []);
 
 
     return (<div>
-        <Users  items={users} appFn={appFn}/>
+        <Users items={users} appFn={appFn}/>
+        <hr/>
+        {
+            user && <div>{JSON.stringify(user)}</div>
+        }
+        <hr/>
     </div>)
 }
 
