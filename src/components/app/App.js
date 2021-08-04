a9eb6e1b22e359d25b5b19d39c7799b90918ccfc
andrii00 <kachmarik00@gmail.com>
2021-08-04 17:31:29 +0300

    redux-1

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,37 +1,96 @@
 import './App.css';
-import {useState} from "react";
+import {useDispatch, useSelector} from 'react-redux'
+import {useEffect, useState} from "react";
 
-export function App() {
-// let [login, setLogin] = useState('')
-// let [password, setPassword] = useState('')
-//     function onLoginChange(e) {
-//         setLogin(e.target.value)
-//     }
-//     function onPasswordChange(e) {
-//         setPassword(e.target.value)
-//     }
-let [formState, setFormState] = useState({login:'', password:''})
-    function onSubmit(e){
-        console.log(e.target.login.value)
-        console.log(e.target.password.value)
-        e.preventDefault()
-        console.log(formState)
+const NestedChild = () => {
+
+    const counter = useSelector(({counter: {value}}) => value)
+    const dispatch = useDispatch();
+    const posts = useSelector(({posts}) => posts)
+
+    const fetchPosts = async () => {
+        const responsePromise = await fetch('https://jsonplaceholder.typicode.com/posts')
+        const json = await responsePromise.json()
+
+        dispatch({
+            type: 'ADD_POSTS',
+            payload: json
+        })
     }
 
- function onChange(e){
+    useEffect(() => {
+        fetchPosts()
+    }, [])
 
-     setFormState({...formState, [e.target.name]: e.target.value})
- }
+    console.log(posts);
 
+    let [inputNumers, setInputNumbers] = useState(null);
+
+    function onSubmit(e) {
+        e.preventDefault()
+    }
+
+    function onChange(e) {
+        setInputNumbers(e.target.value)
+
+    }
 
     return (
-     <div>
-         <form onSubmit={onSubmit}>
-             <input type="text" name={'login'} value={formState.login} onChange={onChange}/>
-             <input type="text" name={'password'} value={formState.password} onChange={onChange}/>
-             <button>submit</button>
-         </form>
-     </div>
+        <div>
+
+            <div>
+                <form onSubmit={onSubmit}>
+                    <input type="number" name={'inputNumbers'} onChange={onChange}/>
+                    <button onClick={() => {
+                        dispatch({
+                            type: 'INPUT_NUMBER',
+                            payload: inputNumers
+                        })
+                    }}>input numbers
+                    </button>
+                </form>
+            </div>
+
+
+            <h1>{counter}</h1>
+            <hr/>
+            <button onClick={() => {
+                dispatch({type: 'INC'})
+            }
+            }>
+                inc
+            </button>
+            <button onClick={() => {
+                dispatch({type: 'DEC'})
+            }
+            }>
+                dec
+            </button>
+            <button onClick={() => {
+                dispatch({type: 'RES'})
+            }
+            }>
+                RES
+            </button>
+
+            <ul>
+                {posts.map(post => (
+                    <li key={post.id}>
+                        {post.id} - {post.title}
+                    </li>
+                ))}
+            </ul>
+
+        </div>
+
+    )
+}
+
+export function App() {
+    return (
+        <div>
+            <NestedChild/>
+        </div>
     )
 }
 
---
+++ src/index.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/app/App';
+import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+
+const initialState = {
+    counter: {
+        value: 0
+    },
+    posts:[]
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INC': {
+            return {
+                ...state,
+                counter: {
+                    value: state.counter.value + 1
+                }
+            }
+        }
+        case 'DEC': {
+            return {
+                ...state,
+                counter: {
+                    value: state.counter.value - 1
+                }
+            }
+        }
+        case 'RES': {
+            return {
+                ...state,
+                counter: {
+                    value: state.counter.value = 0
+                }
+            }
+        }
+
+        case 'ADD_POSTS':{
+            return {
+                ...state,
+                posts:action.payload
+            }
+        }
+
+        case 'INPUT_NUMBER':{
+            return {
+                ...state,
+                counter: {
+                    value: state.counter.value = action.payload
+                }
+            }
+        }
+
+
+        default:
+            return state
+    }
+}
+
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
